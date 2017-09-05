bb6fca8f35880bcde4319b0517ddcac85ae4cfa6
timon-w <timonw@gmail.com>
2017-09-06 09:23:52 +1200

    first commit

---
+++ iot-trap.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid = "FRITZ!Box Fon WLAN 7360";
+const char* password = "xxxx";
+const char* Host = "hooks.slack.com";
+const String SlackHookUrl = "/services/xxx";
+const String SlackIcon = ":rat:";
+const String SlackMessageSet = "@timon, your trap is set :slightly_smiling_face:";
+const String SlackMessageOff = "@timon, your trap has gone off :skull_and_crossbones:";
+const String SlackUsername = "iot-trap";
+const int httpsPort = 443;
+const int TripWire = 5;
+int StateChange = false;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(TripWire, INPUT_PULLUP);
+}
+
+void loop() {
+  // read the input pin:
+  int TrapState = digitalRead(TripWire);
+  Serial.println(TrapState);
+
+  if (TrapState == 1 && StateChange == true){
+    int CallItIn;
+    CallItIn = FunctionPhoneHome(SlackMessageOff);
+    StateChange = false;
+    Serial.println("Trap has gone off");
+  }
+  if (TrapState == 0 && StateChange == false){
+    int CallItIn;
+    CallItIn = FunctionPhoneHome(SlackMessageSet);
+    StateChange = true;
+    Serial.println("Trap has been set");
+  }
+  // delay in between reads
+  delay(3600000);
+}
+
+int FunctionPhoneHome(String Message){
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Connecting to ");
+  Serial.println(Host);
+
+  WiFiClientSecure client;
+
+  if (!client.connect(Host, httpsPort)) {
+    Serial.println("Connection failed :-(");
+  }
+
+  Serial.print("Posting to URL: ");
+  Serial.println(SlackHookUrl);
+
+  String PostData="payload={\"link_names\": 1, \"icon_emoji\": \"" + SlackIcon + "\", \"username\": \"" + SlackUsername + "\", \"text\": \"" + Message + "\"}";
+
+  client.print(String("POST ") + SlackHookUrl + " HTTP/1.1\r\n" +
+               "Host: " + Host + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n" +
+               "Connection: close" + "\r\n" +
+               "Content-Length:" + PostData.length() + "\r\n" +
+               "\r\n" + PostData);
+  Serial.println("Request sent");
+  String Line = client.readStringUntil('\n');
+  Serial.printf("Response code was: ");
+  Serial.println(Line);
+  Serial.println("closing connection");
+  WiFi.disconnect();
+  delay(100);
+  WiFi.mode(WIFI_OFF);
+}
